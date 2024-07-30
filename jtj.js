397a8ec7d32da7c35fc23a95ee3341cbebfdf67c
Ellie Nash <ellienashmo@gmail.com>
2024-07-30 16:02:48 -0500

    Score handling and start menu

--- jtj.js
+++ jtj.js
@@ -1,3 +1,40 @@
+//Game variable declarations
+var score = 0;
+var post_verdict; //Global variable for post verdict
+var user_verdict; //Global variable for user verdict
+const Verdicts = {
+  //Possible post verdicts, correlated with button text
+  YTA: "Asshole",
+  NTA: "Not the A-hole",
+  ESH: "Everyone Sucks",
+  NAH: "No A-holes here",
+};
+
+//Function to start new game
+$(document).ready(function () {
+  $("#newgame").click(function () {
+    $(this).animate(
+      {
+        width: "9.5vw",
+        height: "7.5vh",
+        margin: "3.75vw",
+        "border-radius": "4.75vh",
+      },
+      40
+    );
+    $(this).animate(
+      {
+        width: "10vw",
+        height: "8vh",
+        margin: "3.5vw",
+        "border-radius": "4.5vh",
+      },
+      40
+    );
+    new_game();
+  });
+});
+
 //Function to handle judgement button click
 $(document).ready(function () {
   $(".judgement").click(function () {
@@ -20,6 +57,16 @@ $(document).ready(function () {
       },
       40
     );
+
+    //Compare button text to post verdict
+    var user_verdict = String($(this).text());
+    if (user_verdict == post_verdict) {
+      score++;
+      $("#scorenum").text(score);
+      //Generate new post
+    } else {
+      game_over();
+    }
   });
 });
 
@@ -27,15 +74,32 @@ $(document).ready(function () {
 function input_post(url) {
   //Wait for GET request to complete
   $.when(get_AITA_post(url)).done(function (data) {
+    //Parse post JSON for title, text, and majority verdict
     const post_title = data[0]["data"]["children"]["0"]["data"]["title"];
-    const post_text =
-      data[0]["data"]["children"]["0"]["data"]["selftext"];
-    const post_verdict =
-      data[0]["data"]["children"]["0"]["data"]["link_flair_text"];
+    const post_text = data[0]["data"]["children"]["0"]["data"]["selftext"];
+    post_verdict = data[0]["data"]["children"]["0"]["data"]["link_flair_text"];
 
+    //Populate appropriate fields
     $("#titletext").text(post_title);
     $("#posttext").text(post_text);
-    console.log(post_verdict);
+
+    //Convert verdict to shorthand
+    switch (post_verdict) {
+      case Verdicts.ESH:
+        post_verdict = "ESH";
+        break;
+      case Verdicts.NAH:
+        post_verdict = "NAH";
+        break;
+      case Verdicts.NTA:
+        post_verdict = "NTA";
+        break;
+      case Verdicts.YTA:
+        post_verdict = "YTA";
+        break;
+      default:
+      //Generate new post
+    }
   });
 }
 
@@ -45,5 +109,17 @@ function get_AITA_post(url) {
 }
 
 input_post(
-  "https://www.reddit.com/r/AmItheAsshole/comments/1e5s7kl/aita_for_not_giving_my_daughter_a_car_for_her/.json"
+  "https://www.reddit.com/r/AmItheAsshole/comments/1efan4h/aita_for_telling_my_fil_that_i_dont_want_the/.json"
 );
+
+function new_game() {
+  score = 0;
+  $("#scorenum").text(score);
+  $("#freezeoverlay").fadeOut(300);
+
+  //Get new post
+}
+
+function game_over() {
+  $("#freezeoverlay").fadeIn(100);
+}
